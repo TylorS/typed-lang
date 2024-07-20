b995b1e2ba13c8f8473ff4ecbe0ebd9550966d53
Tylor Steinberger <tlsteinberger167@gmail.com>
2024-07-20 19:32:08 -0400

    WIP: remove expression statement, return statement only in blocks

--- packages/parser/src/AST/Nodes/Block.ts
+++ packages/parser/src/AST/Nodes/Block.ts
@@ -1,11 +1,18 @@
 import { Span } from "../../Span.js";
+import { Expression } from "../Expression.js";
 import { Statement } from "../Statement.js";
 
 export class Block {
   readonly _tag = "Block";
 
   constructor(
-    readonly statements: ReadonlyArray<Statement>,
+    readonly statements: ReadonlyArray<Statement | ReturnStatement>,
     readonly span: Span
   ) {}
+}
+
+export class ReturnStatement {
+  readonly _tag = "ReturnStatement";
+
+  constructor(readonly keyword: Span, readonly expression: Expression) {}
 }
--- packages/parser/src/AST/Statement.ts
+++ packages/parser/src/AST/Statement.ts
@@ -1,10 +1,8 @@
-import { Span } from "../Span.js";
 import { BrandDeclaration } from "./Declaration/BrandDeclaration.js";
 import { DataDeclaration } from "./Declaration/DataDeclaration.js";
 import { FunctionDeclaration } from "./Declaration/FunctionDeclaration.js";
 import { TypeAliasDeclaration } from "./Declaration/TypeAliasDeclaration.js";
 import { VariableDeclaration } from "./Declaration/VariableDeclaration.js";
-import { Expression } from "./Expression.js";
 
 export type Statement =
   | BrandDeclaration
@@ -12,17 +10,3 @@ export type Statement =
   | FunctionDeclaration
   | TypeAliasDeclaration
   | VariableDeclaration
-  | ExpressionStatement
-  | ReturnStatement;
-
-export class ExpressionStatement {
-  readonly _tag = "ExpressionStatement";
-
-  constructor(readonly expression: Expression) {}
-}
-
-export class ReturnStatement {
-  readonly _tag = "ReturnStatement";
-
-  constructor(readonly keyword: Span, readonly expression: Expression) {}
-}
