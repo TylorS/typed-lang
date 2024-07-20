c85483cd23877604f01019dd7e8cd434bfca8409
Tylor Steinberger <tlsteinberger167@gmail.com>
2024-07-20 03:23:02 -0400

    chore: add a second data type

--- examples/data-types/src/main.ts
+++ examples/data-types/src/main.ts
@@ -1,4 +1,7 @@
-import { Maybe } from "./Maybe.typed";
+import { Either, Maybe } from "./data.typed";
 
 console.log(Maybe.Nothing);
 console.log(Maybe.Just(1));
+
+console.log(Either.Left("uh-oh"));
+console.log(Either.Right(1));
