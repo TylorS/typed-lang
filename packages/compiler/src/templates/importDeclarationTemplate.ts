2180f71c928aa3aa8678fb079b07ebad4e8218e9
Tylor Steinberger <tlsteinberger167@gmail.com>
2024-07-21 22:21:01 -0400

    WIP: import declaration template

--- packages/compiler/src/templates/importDeclarationTemplate.ts
+++ packages/compiler/src/templates/importDeclarationTemplate.ts
@@ -1,8 +1,29 @@
 import { ImportDeclaration } from "@typed-lang/parser";
-import { Interpolation } from "../Template.js";
+import { Interpolation, t } from "../Template.js";
 
 export function importDeclarationTemplate(
   decl: ImportDeclaration
 ): Interpolation {
-  throw new Error("Not implemented");
+  switch (decl.imports._tag) {
+    case "NamedImports":
+      return t.span(decl.span)(
+        t`import { `,
+        t.intercolate(`, `)(
+          decl.imports.imports.map((i) =>
+            i.alias
+              ? t`${t.identifier(i.name)} as ${t.identifier(i.alias)}`
+              : t.identifier(i.name)
+          )
+        ),
+        t` } from `,
+        t.span(decl.moduleSpecifier.span)(`${decl.moduleSpecifier.value}`)
+      );
+    case "NamespaceImport":
+      return t.span(decl.span)(
+        t`import * as `,
+        t.identifier(decl.imports.name),
+        t` from `,
+        t.span(decl.moduleSpecifier.span)(`${decl.moduleSpecifier.value}`)
+      );
+  }
 }
