4d4c81813a9417b889e4fe89a52b86bcb5bc6dbf
Tylor Steinberger <tlsteinberger167@gmail.com>
2024-07-21 22:21:13 -0400

    WIP: type alias declaration template

--- packages/compiler/src/templates/typeAliasDeclarationTemplate.ts
+++ packages/compiler/src/templates/typeAliasDeclarationTemplate.ts
@@ -1,8 +1,15 @@
 import { TypeAliasDeclaration } from "@typed-lang/parser";
 import { Interpolation } from "../Template.js";
+import { typeAliasTemplate } from "./typeAliasTemplate.js";
+import { typeTemplate } from "./typeTemplate.js";
 
 export function typeAliasDeclarationTemplate(
   decl: TypeAliasDeclaration
 ): Interpolation {
-  throw new Error("Not implemented");
+  return typeAliasTemplate({
+    name: decl.name,
+    typeParams: decl.typeParameters,
+    types: [typeTemplate(decl.type)],
+    exported: decl.exported,
+  });
 }
